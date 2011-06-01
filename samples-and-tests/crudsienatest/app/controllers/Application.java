f34f739867575d8220a8dd007267e38f6e556e3f
pascal <pascal@Pascal-Portable>
2011-06-01 17:56:56 +0200

    updated samples (this was for smallufo on the mailing list)

--- samples-and-tests/crudsienatest/app/controllers/Application.java
+++ samples-and-tests/crudsienatest/app/controllers/Application.java
@@ -13,4 +13,34 @@ public class Application extends Controller {
         render();
     }
 
+    public static void fourword(){
+    	Fourword fw = new Fourword();
+    	fw.test = "alpha";
+    	// the save function is a new function which means "SAVE OR UPDATE"
+    	fw.save();
+    	Logger.debug("Fourword = %s ", fw);
+    	FourwordUser user = new FourwordUser();
+    	user.birthdate = new Date();
+    	user.uid = "1234";
+    	user.fourword = fw;
+    	// the save function is a new function which means "SAVE OR UPDATE"
+    	user.save();
+    	
+    	Fourword fwu = FourwordUser.getFourwords("1234");
+    	renderText(fwu);
+    }
+    
+    public static void fourword2(){
+    	
+    	FourwordUser user = new FourwordUser();
+    	user.birthdate = new Date();
+    	user.uid = "1234";
+    	user.insert();
+    	user.fourword = Fourword.all().filter("test", "alpha").get();
+    	// the save function is a new function which means "SAVE OR UPDATE"
+    	user.save();
+    	
+    	Fourword fwu = FourwordUser.getFourwords("1234");
+    	renderText(fwu);
+    }
 }
--- samples-and-tests/crudsienatest/app/models/Fourword.java
+++ samples-and-tests/crudsienatest/app/models/Fourword.java
@@ -3,6 +3,7 @@ package models;
 import siena.Generator;
 import siena.Id;
 import siena.Model;
+import siena.Query;
 
 public class Fourword extends Model {
 	@Id(Generator.AUTO_INCREMENT)
@@ -11,6 +12,14 @@ public class Fourword extends Model {
 	public String test;
 
 	public static Fourword getFourword(Long id2) {
-		return Model.all(Fourword.class).getByKey(id2);
+		return all().getByKey(id2);
+	}
+	
+	public String toString() {
+		return "id:"+this.id+" - test:"+this.test;
+	}
+	
+	public static Query<Fourword> all() {
+		return Model.all(Fourword.class);
 	}
 }
